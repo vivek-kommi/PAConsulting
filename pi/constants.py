4ec6a3bb939e677697826311fe351b6c3a1c1278
Sai5767 <77236137+Sai5767@users.noreply.github.com>
2021-01-30 10:37:09 +0000

    Add files via upload

---
+++ pi/constants.py
@@ -0,0 +1,6 @@
+YOLOV3_LABELS_PATH = 'C:/Users/prana/OneDrive/Documents/Python Scripts/pi/yolov3/coco.names'
+YOLOV3_CFG_PATH = 'C:/Users/prana/OneDrive/Documents/Python Scripts/pi/yolov3/yolov3.cfg'
+YOLOV3_WEIGHTS_PATH = 'C:/Users/prana/OneDrive/Documents/Python Scripts/pi/yolov3/yolov3.weights'
+VIDEO_PATH = 'C:/Users/prana/OneDrive/Documents/Python Scripts/pi/videos/video.mp4'
+OUTPUT_PATH = 'C:/Users/prana/OneDrive/Documents/Python Scripts/pi/output/output.avi'
+SAFE_DISTANCE = 60
